6569adefc8fcb025e49ed4468acf907c3d1d53ec
dcmicas <dcmicas@gmail.com>
2022-08-21 10:45:26 -0300

    Fundamentos de Java

---
+++ Java/MeuPrimeiroProg/src/main/java/EntradaDados.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class EntradaDados {
+    public static void main (String [] args){
+        //Entrada de  Dados via JOPtionPane
+
+        int idade;
+
+        idade = Integer.parseInt(JOptionPane.showInputDialog("Informe a idade da pessoa: "));
+    }
+
+}
---
+++ Java/MeuPrimeiroProg/src/main/java/Literais.java
@@ -0,0 +1,18 @@
+public class Literais {
+    public static void main (String [] args){
+        int numero;
+        String cadeiaCaracteres;
+        char caracter;
+        boolean certoOuErrado;
+
+        numero = 10;
+        cadeiaCaracteres = "Meu curso de Java";
+        caracter='S';
+        certoOuErrado = false;
+
+        System.out.println("O valor de número é " + numero);
+        System.out.println("O valor de cadeiaCaracteres é " + cadeiaCaracteres);
+        System.out.println("O valor de caracter é " + caracter);
+        System.out.println("O valor de certoOuErrado é " + certoOuErrado);
+    }
+}
